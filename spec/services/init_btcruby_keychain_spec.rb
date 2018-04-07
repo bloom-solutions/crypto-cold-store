771b41e99778583cc16a57295a9043eb62100e10
Ramon Tayag <ramon.tayag@gmail.com>
2018-04-07 15:14:26 +0800

    Fix spacing

--- spec/services/init_btcruby_keychain_spec.rb
+++ spec/services/init_btcruby_keychain_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe InitBtcrubyKeychain do
 
-  let(:master_public_key) do "xpub6FMbbkESWrcdwWdJgfW4qPMF8JhjTbLgKoSjKofd6X5f1rjz1qm3N3nQ4DfFcz8SHPNrVq4RzfmvRG488LPY7d59r4Cp4bV7e7G6zvUA8MU"
+  let(:master_public_key) do
+    "xpub6FMbbkESWrcdwWdJgfW4qPMF8JhjTbLgKoSjKofd6X5f1rjz1qm3N3nQ4DfFcz8SHPNrVq4RzfmvRG488LPY7d59r4Cp4bV7e7G6zvUA8MU"
   end
 
   it "places a #{BTC::Keychain} in the context with the master_public_key" do
