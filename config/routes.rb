d766eda5d3dad7ec92cbfa89712c359a48d9693b
Ramon Tayag <ramon.tayag@gmail.com>
2018-04-16 10:47:24 +0800

    Mount sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 require 'sidekiq/api'
+require 'sidekiq/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,6 +8,21 @@ Rails.application.routes.draw do
       resource :deposit_detail, only: %i[show]
     end
   end
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    given_username_hash = ::Digest::SHA256.hexdigest(username)
+    username_hash = ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])
+    given_password_hash = ::Digest::SHA256.hexdigest(password)
+    password_hash = ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"])
+
+    username_comparison = ActiveSupport::SecurityUtils.
+      secure_compare(given_username_hash, username_hash)
+    password_comparison = ActiveSupport::SecurityUtils.
+      secure_compare(given_password_hash, password_hash)
+
+    username_comparison & password_comparison
+  end if Rails.env.production?
+  mount Sidekiq::Web => '/sidekiq'
 
   match({
     "queue-status" => proc {
