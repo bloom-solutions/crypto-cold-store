63e5d909e520d05da56ec41ebe72bf3973f1b401
Ramon Tayag <ramon.tayag@gmail.com>
2018-04-16 11:16:55 +0800

    Mount sidekiq-cron/web

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 require 'sidekiq/api'
 require 'sidekiq/web'
+require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
