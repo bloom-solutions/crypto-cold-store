469d32aaf791ac1a537b1c6ec85f164d10ed68c0
Ramon Tayag <ramon.tayag@gmail.com>
2018-04-16 10:38:33 +0800

    Expose /queue-status for external monitoring of Sidekiq
    https://github.com/mperham/sidekiq/wiki/Monitoring#monitoring-queue-backlog

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/api'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
@@ -5,4 +7,15 @@ Rails.application.routes.draw do
       resource :deposit_detail, only: %i[show]
     end
   end
+
+  match({
+    "queue-status" => proc {
+      [
+        200,
+        {"Content-Type" => "text/plain"},
+        [Sidekiq::Queue.new.size < 100 ? "OK" : "UHOH"],
+      ]
+    },
+    via: :get,
+  })
 end
