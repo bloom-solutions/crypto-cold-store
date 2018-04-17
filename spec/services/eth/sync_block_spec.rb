c5bfa226dfb2806b3e8ae9ce34be236cfadf3efa
Ramon Tayag <ramon.tayag@gmail.com>
2018-04-17 20:19:08 +0800

    Test `Eth::SyncBlock` deletes blocks in forks

--- spec/services/eth/sync_block_spec.rb
+++ spec/services/eth/sync_block_spec.rb
@@ -10,11 +10,17 @@ module Eth
       })
     end
 
+    before do
+      create(:block, coin: "eth", height: 2952850, block_hash: "something-else")
+    end
+
     it "syncs the block txs of known addresses" do
       described_class.(2952850)
       tx = ::Tx.of_coin(:eth).
         find_by(tx_id: "0x635e994ceeecf74731a5aeb1b43d29f024ab2a6c1b234328b5a1bc996b6a01e6")
 
+      block_in_fork = Block.eth.find_by(block_hash: "something-else")
+      expect(block_in_fork).to be_nil
       expect(tx).to be_present
       expect(tx.block).to be_present
       expect(tx.amount.round(18).to_f).to eq 0.229788202932067616
