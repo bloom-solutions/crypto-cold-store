342c36681069d1ab8330ff85f467b38ba62a2ca1
Ramon Tayag <ramon.tayag@gmail.com>
2018-04-17 20:33:44 +0800

    MessageBus by default retains up to 10 million messages up to 10 days

--- config/initializers/message_bus.rb
+++ config/initializers/message_bus.rb
@@ -5,4 +5,14 @@ else
     backend: :postgres,
     backend_options: ENV["MESSAGE_BUS_PG_CONNECTION_STRING"],
   })
+
+  # 10 days in seconds
+  MessageBus.reliable_pub_sub.max_backlog_age =
+    ENV.fetch("MESSAGE_BUS_MAX_BACKLOG_AGE") { 10 * 24 * 60 * 60 }.to_i
+
+  MessageBus.reliable_pub_sub.max_backlog_size =
+    ENV.fetch("MESSAGE_BUS_MAX_BACKLOG_SIZE") { 10_000_000 }.to_i
+
+  MessageBus.reliable_pub_sub.max_global_backlog_size =
+    ENV.fetch("MESSAGE_BUS_MAX_GLOBAL_BACKLOG_SIZE") { 10_000_000 }.to_i
 end
