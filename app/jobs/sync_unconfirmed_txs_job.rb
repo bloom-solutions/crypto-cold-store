a6533027fb6cefe36143ad0228e69564ea75ea1f
Ramon Tayag <ramon.tayag@gmail.com>
2018-10-27 22:09:34 +0800

    Log on conflict of unique job

--- app/jobs/sync_unconfirmed_txs_job.rb
+++ app/jobs/sync_unconfirmed_txs_job.rb
@@ -1,6 +1,6 @@
 class SyncUnconfirmedTxsJob < ApplicationJob
 
-  sidekiq_options unique: :until_executed
+  sidekiq_options unique: :until_executed, on_conflict: :log
 
   def perform(coin)
     coin.classify.constantize.const_get("SyncUnconfirmedTxs").()
