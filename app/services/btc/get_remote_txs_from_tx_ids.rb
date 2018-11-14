37e7bec212320ff4fa6d31e011a720fcbcde4dcc
Ramon Tayag <ramon.tayag@gmail.com>
2018-11-14 13:36:20 +1100

    Convert syncing of unconfirmed btc txs to batch call

---
+++ app/services/btc/get_remote_txs_from_tx_ids.rb
@@ -0,0 +1,16 @@
+module Btc
+  class GetRemoteTxsFromTxIds
+
+    extend LightService::Action
+    expects :bitcoiner_client, :tx_ids
+    promises :remote_txs
+    VERBOSE = true
+
+    executed do |c|
+      args = c.tx_ids.map { |tx_id| ["getrawtransaction", [tx_id, VERBOSE]] }
+      response = c.bitcoiner_client.request(args)
+      c.remote_txs = response.map { |r| r["result"] }
+    end
+
+  end
+end
