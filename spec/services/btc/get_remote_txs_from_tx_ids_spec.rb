c2985cad3703a5557bb852cbf85a9e5b44ce12de
Ramon Tayag <ramon.tayag@gmail.com>
2018-11-21 12:07:00 +0800

    Remove nil `remote_txs`
    In production there are instances when some of the remote_txs are nil.
    
    I suspect it's because some of the fetches fail. Perhaps they've been dropped
    from the mempool, and thus the `result` field is `null`.

--- spec/services/btc/get_remote_txs_from_tx_ids_spec.rb
+++ spec/services/btc/get_remote_txs_from_tx_ids_spec.rb
@@ -8,16 +8,18 @@ module Btc
       [
         "53c38b761f228332fa54a48697441be969eb727d48ab5c1b9f5dcec89bea2216",
         "d0d9f56a9eff55c0b40a3fe696d080d9d900de9203bf2c3fc014e15756ce1a97",
+        "abc",
       ]
     end
 
-    it "fetches the tx details from the array of tx_ids", vcr: {record: :once} do
+    it "fetches the tx details from the array of tx_ids, removing any nils", vcr: {record: :once} do
       resulting_ctx = described_class.execute({
         bitcoiner_client: bitcoiner_client,
         tx_ids: tx_ids,
       })
 
       remote_txs = resulting_ctx.remote_txs
+      expect(remote_txs.size).to eq 2
 
       expect(remote_txs[0]["txid"]).to eq tx_ids[0]
       expect(remote_txs[0]["vout"][0]["value"]).to eq 0.325
